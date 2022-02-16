37cb73f094fc602e98f8f6638f62ac147a9bd5f3
Firman Lestari <jusles363@gmail.com>
2022-02-16 07:49:21 +0700

    feat: connect service with controller

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -1,42 +1,58 @@
 import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
+import { BookService } from './book.service';
 import { AddUpdateBookDto } from './dto/add-update-book.dto';
 import { GetWithColumnDto } from './dto/get-with-column.dto';
 
 @Controller('book')
 export class BookController {
+  constructor(private readonly bookService: BookService) {}
   
   @Get('/search-column')
   async getWithColumn(@Query() data: GetWithColumnDto) {
-    
+    return {
+      'data': await this.bookService.getWithColumn(data)
+    }
   }
 
   @Get('/')
   async getAll() {
-
+    return {
+      'data': await this.bookService.getAll()
+    }
   }
 
   @Get('/:id')
   async get(@Param('id') id) {
-
+    return {
+      'data': await this.bookService.get(id)
+    }
   }
 
   @Get('/search/:search')
   async search(@Param('search') search) {
-
+    return {
+      'data': await this.bookService.search(search)
+    }
   }
 
   @Post('/add')
   async add(@Body() data: AddUpdateBookDto) {
-
+    return {
+      'data': await this.bookService.add(data)
+    }
   }
 
   @Put('/update/:id')
   async update(@Param('id') id, @Body() data: AddUpdateBookDto) {
-
+    return {
+      'data': await this.bookService.update(id, data)
+    }
   }
 
   @Delete('/delete/:id')
   async delete(@Param('id') id) {
-    
+    return {
+      'data': await this.bookService.delete(id)
+    }
   }
 }
